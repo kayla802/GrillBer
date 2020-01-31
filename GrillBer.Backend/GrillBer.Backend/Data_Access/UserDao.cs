2a491b34780177763398c64c1b4afe84031875ad
kayla802 <57269429+kayla802@users.noreply.github.com>
2020-01-31 15:22:06 -0600

    Added UserDao.cs

---
+++ GrillBer.Backend/GrillBer.Backend/Data_Access/UserDao.cs
@@ -0,0 +1,33 @@
+﻿using GrillBer.Backend.Data_Acess;
+using GrillBer.Backend.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GrillBer.Backend.Data_Access
+{
+    public class UserDao: DaoBase
+
+    {
+        public User NewUser(User user)
+        {
+            //using (var db = new LiteDatabase(GrillBerDBLocation))
+            //{
+            //    var userCol = db.GetCollection<User>("Users");
+            //    var newUser = new User()
+            //    {
+            //        Id = Guid.NewGuid(),
+            //        Username = user.Username,
+            //        FirstName = user.FirstName,
+            //        LastName = user.LastName
+            //    };
+
+            //    userCol.Insert(newUser);
+            //    return newUser;
+            //}
+            return null;
+
+        }
+    }
+}
