217e5ffc66b85a32556e5373b906921b0dde4601
andrewmcampgit <andrew.camp@veteransunited.com>
2020-01-31 15:05:47 -0600

    Create Rental.cs

---
+++ GrillBer.Backend/GrillBer.Backend/Models/Rental.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GrillBer.Backend.Models
+{
+    public class Rental
+    {
+        public Guid Id { get; set; }
+        public Guid User { get; set; }
+        public Guid Grill { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+    }
+}
