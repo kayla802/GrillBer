a0cf90b10821e58d0a812bc6b2604e9a0428f190
rmmartin08 <51770009+rmmartin08@users.noreply.github.com>
2020-01-31 14:43:09 -0600

    Create DaoBase.cs
    added the Data_Access folder and made the main DaoBase

---
+++ GrillBer.Backend/GrillBer.Backend/Data_Access/DaoBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace GrillBer.Backend.Data_Acess
+{
+    public class DaoBase
+    {
+
+        public const string GrillBerDBLocation = @"C:\GrillBer\Grillber.db";
+
+        public DaoBase()
+        {
+            var grillberDBDir = Path.GetDirectoryName(GrillBerDBLocation);
+            if (grillberDBDir != null && !Directory.Exists(grillberDBDir))
+            {
+                Directory.CreateDirectory(grillberDBDir);
+            }
+        }
+    }
+}
