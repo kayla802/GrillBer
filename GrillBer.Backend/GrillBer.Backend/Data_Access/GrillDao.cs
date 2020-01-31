cab8dfa02e1f7198a39a174a4a1f5dcdaa057507
rmmartin08 <51770009+rmmartin08@users.noreply.github.com>
2020-01-31 16:01:12 -0600

    Update GrillDao.cs

--- GrillBer.Backend/GrillBer.Backend/Data_Access/GrillDao.cs
+++ GrillBer.Backend/GrillBer.Backend/Data_Access/GrillDao.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using LiteDB;
 
 namespace GrillBer.Backend.Data_Access
 {
@@ -14,8 +15,38 @@ namespace GrillBer.Backend.Data_Access
             using (var db = new LiteDatabase(GrillBerDBLocation))
             {
                 var grillcol = db.GetCollection<Grill>("Grills");
-                return grillcol.FindByID(grillID);
+                return grillcol.FindById(grillID);
             }
         }
+
+        public Grill CreateNewGrill(Grill inGrill)
+        {
+            using (var db = new LiteDatabase(GrillBerDBLocation))
+            {
+                var grillCol = db.GetCollection<Grill>("Grills");
+                var userDao = new UserDao();
+                var foundUser = userDao.GetSingleUserById(owner);
+                if (foundUser == null)
+                {
+                    throw new KeyNotFoundException("User doesn't exist: " + owner.ToSting());
+                }
+                var newGrill = new Grill()
+                {
+                    Id = Guid.NewGuid(),
+                    Brand = inGrill.Brand,
+                    Model = inGrill.Model,
+                    City = inGrill.City,
+                    Cost = inGrill.Cost,
+                    Rating = inGrill.Rating,
+                    Owner = inGrill.Owner,
+                    RenterId = inGrill.RenterId
+                };
+                grillCol.Insert(newGrill);
+                return newGrill;
+            
+                
+            }
+        }
+
     }
 }
