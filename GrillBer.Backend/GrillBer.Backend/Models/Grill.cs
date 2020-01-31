38c8f4fc952ebd6bd9b82168cdc3ee7561a12dbd
andrewmcampgit <andrew.camp@veteransunited.com>
2020-01-31 14:31:24 -0600

    Made Grill Model

---
+++ GrillBer.Backend/GrillBer.Backend/Models/Grill.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GrillBer.Backend.Models
+{
+    public class Grill
+    {
+        public Guid Id { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public string City { get; set; }
+        public int Cost { get; set; }
+        public int Rating { get; set; }
+        public Guid Owner { get; set; }
+        public Guid RenterId { get; set; }
+    }
+}
