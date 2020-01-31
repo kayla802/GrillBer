b9afe52b6d9cfea6f1b1c99f455afdfc707f5e33
andrewmcampgit <andrew.camp@veteransunited.com>
2020-01-31 16:38:31 -0600

    Make RentalController

---
+++ GrillBer.Backend/GrillBer.Backend/Controllers/RentalController.cs
@@ -0,0 +1,25 @@
+﻿using GrillBer.Backend.Data_Access;
+using GrillBer.Backend.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace GrillBer.Backend.Controllers
+{
+    [RoutePrefix("api/rentals")]
+    public class RentalController : ApiController
+    {
+                
+            [HttpPost]
+        public Rental NewRental(Rental inRental)
+        {
+            RentalDao rentalDao = new RentalDao();
+            return RentalDao.newRental(inRental);
+        }
+
+    }
+}
+
