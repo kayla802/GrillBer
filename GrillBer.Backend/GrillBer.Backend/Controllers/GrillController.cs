f34e25be065b68e91350dd82aca5594b705d7f0e
leighmaj <51490479+leighmaj@users.noreply.github.com>
2020-02-14 13:58:24 -0600

    adding missing files from previous commit

--- GrillBer.Backend/GrillBer.Backend/Controllers/GrillController.cs
+++ GrillBer.Backend/GrillBer.Backend/Controllers/GrillController.cs
@@ -20,41 +20,41 @@ namespace GrillBer.Backend.Controllers
             return grillDao.GetSingleGrillById(grillId);
         }
 
-        [HttpGet]
-        public Grill[] GetGrills(string brand = null, string model = null, string city = null, int? cost = null, int? rating = null)
-        {
-            var grillDao = new GrillDao();
-            IEnumerable<Grill> grills = grillDao.GetGrills();
-            
-            if (!string.IsNullOrWhiteSpace(brand))
-            {
-                grills = grills.Where(g => g.Brand.Contains(brand));
-            }
+		[HttpGet]
+		public Grill[] GetGrills(string brand = null, string model = null, string city = null, int? cost = null, int? rating = null)
+		{
+			var grillDao = new GrillDao();
+			IEnumerable<Grill> grills = grillDao.GetGrills();
 
-            if (!string.IsNullOrWhiteSpace(model))
-            {
-                grills = grills.Where(g => g.Model.Contains(model));
-            }
+			if (!string.IsNullOrWhiteSpace(brand))
+			{
+				grills = grills.Where(g => g.Brand.Contains(brand));
+			}
 
-            if (!string.IsNullOrWhiteSpace(city))
-            {
-                grills = grills.Where(g => g.City.Contains(city));
-            }
+			if (!string.IsNullOrWhiteSpace(model))
+			{
+				grills = grills.Where(g => g.Model.Contains(model));
+			}
 
-            if (cost.HasValue)
-            {
-                grills = grills.Where(g => g.Cost.Equals(cost));
-            }
+			if (!string.IsNullOrWhiteSpace(city))
+			{
+				grills = grills.Where(g => g.City.Contains(city));
+			}
 
-            if (rating.HasValue)
-            {
-                grills = grills.Where(g => g.Rating.Equals(rating));
-            }
+			if (cost.HasValue)
+			{
+				grills = grills.Where(g => g.Cost.Equals(cost));
+			}
 
-            return grills.ToArray();
-        }
+			if (rating.HasValue)
+			{
+				grills = grills.Where(g => g.Rating.Equals(rating));
+			}
 
-        [HttpPost]
+			return grills.ToArray();
+		}
+
+		[HttpPost]
         public Grill AddNewGrill(Grill inGrill)
         {
             var grillDao = new GrillDao();
